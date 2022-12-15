dadbf033522a24110394a074245f83bafb0a45ac
ras0q <asymptote.k.k@gmail.com>
2022-12-15 19:02:07 +0900

    :recycle: split handleEvent

--- internal/usecases/services/ws/handler.go
+++ internal/usecases/services/ws/handler.go
@@ -11,100 +11,116 @@ import (
 func (h *Hub) handleEvent(req *oapi.WsRequest) error {
 	switch req.Type {
 	case oapi.WsRequestTypeGameStartEvent:
-		_, err := req.Body.AsWsRequestBodyGameStartEvent()
-		if err != nil {
-			return err
-		}
-
-		// TODO: 初期カードを決めるロジックを書く
-		cards := []oapi.Card{
-			{Id: uuid.New(), Type: oapi.CardTypeCreateRail},
-			{Id: uuid.New(), Type: oapi.CardTypeCreateRail},
-			{Id: uuid.New(), Type: oapi.CardTypeCreateBlock},
-			{Id: uuid.New(), Type: oapi.CardTypeCreateBlock},
-			{Id: uuid.New(), Type: oapi.CardTypeCreateBlock},
-		}
-
-		room, err := h.roomRepo.FindRoom(repository.CommonRoomID) // TODO 適切なIDを指定する
-		if err != nil {
-			return err
-		}
-
-		players := make([]oapi.Player, len(room.Players))
-		for i, p := range room.Players {
-			players[i] = oapi.PlayerFromDomain(p)
-		}
-
-		res, err := oapi.NewWsResponseGameStarted(cards, players)
-		if err != nil {
-			return err
-		}
-
-		h.bloadcast(res)
-
-		return nil
+		return h.handleGameStartEvent(req.Body)
 
 	case oapi.WsRequestTypeCardEvent:
-		b, err := req.Body.AsWsRequestBodyCardEvent()
-		if err != nil {
-			return err
-		}
-
-		switch b.Type {
-		case oapi.CardTypeCreateRail:
-			res, err := oapi.NewWsResponseRailCreated()
-			if err != nil {
-				return err
-			}
-
-			h.bloadcast(res)
-
-		case oapi.CardTypeCreateBlock:
-			res, err := oapi.NewWsResponseBlockCreated()
-			if err != nil {
-				return err
-			}
-
-			h.bloadcast(res)
-
-		default:
-			return errors.New("invalid card type")
-		}
-
-		return nil
+		return h.handleCardEvent(req.Body)
 
 	case oapi.WsRequestTypeLifeEvent:
-		b, err := req.Body.AsWsRequestBodyLifeEvent()
-		if err != nil {
-			return err
-		}
-
-		switch b.Type {
-		case oapi.LifeEventTypeDecrement:
-			res, err := oapi.NewWsResponseLifeChanged()
-			if err != nil {
-				return err
-			}
-
-			h.bloadcast(res)
-
-		default:
-			return errors.New("invalid life type")
-		}
-
-		return nil
+		return h.handleLifeEvent(req.Body)
 
 	case oapi.WsRequestTypeRailMergeEvent:
-		res, err := oapi.NewWsResponseRailMerged()
-		if err != nil {
-			return err
-		}
-
-		h.bloadcast(res)
-
-		return nil
+		return h.handleRailMergeEvent(req.Body)
 
 	default:
 		return errors.New("invalid request type")
 	}
+}
+
+func (h *Hub) handleGameStartEvent(body oapi.WsRequest_Body) error {
+	_, err := body.AsWsRequestBodyGameStartEvent()
+	if err != nil {
+		return err
+	}
+
+	// TODO: 初期カードを決めるロジックを書く
+	cards := []oapi.Card{
+		{Id: uuid.New(), Type: oapi.CardTypeCreateRail},
+		{Id: uuid.New(), Type: oapi.CardTypeCreateRail},
+		{Id: uuid.New(), Type: oapi.CardTypeCreateBlock},
+		{Id: uuid.New(), Type: oapi.CardTypeCreateBlock},
+		{Id: uuid.New(), Type: oapi.CardTypeCreateBlock},
+	}
+
+	room, err := h.roomRepo.FindRoom(repository.CommonRoomID) // TODO 適切なIDを指定する
+	if err != nil {
+		return err
+	}
+
+	players := make([]oapi.Player, len(room.Players))
+	for i, p := range room.Players {
+		players[i] = oapi.PlayerFromDomain(p)
+	}
+
+	res, err := oapi.NewWsResponseGameStarted(cards, players)
+	if err != nil {
+		return err
+	}
+
+	h.bloadcast(res)
+
+	return nil
+}
+
+func (h *Hub) handleCardEvent(body oapi.WsRequest_Body) error {
+	b, err := body.AsWsRequestBodyCardEvent()
+	if err != nil {
+		return err
+	}
+
+	var res *oapi.WsResponse
+
+	switch b.Type {
+	case oapi.CardTypeCreateRail:
+		res, err = oapi.NewWsResponseRailCreated()
+		if err != nil {
+			return err
+		}
+
+	case oapi.CardTypeCreateBlock:
+		res, err = oapi.NewWsResponseBlockCreated()
+		if err != nil {
+			return err
+		}
+
+	default:
+		return errors.New("invalid card type")
+	}
+
+	h.bloadcast(res)
+
+	return nil
+}
+
+func (h *Hub) handleLifeEvent(body oapi.WsRequest_Body) error {
+	b, err := body.AsWsRequestBodyLifeEvent()
+	if err != nil {
+		return err
+	}
+
+	switch b.Type {
+	case oapi.LifeEventTypeDecrement:
+		res, err := oapi.NewWsResponseLifeChanged()
+		if err != nil {
+			return err
+		}
+
+		h.bloadcast(res)
+
+	default:
+		return errors.New("invalid life type")
+	}
+
+	return nil
+}
+
+func (h *Hub) handleRailMergeEvent(_ oapi.WsRequest_Body) error {
+	res, err := oapi.NewWsResponseRailMerged()
+	if err != nil {
+		return err
+	}
+
+	h.bloadcast(res)
+
+	return nil
 }
