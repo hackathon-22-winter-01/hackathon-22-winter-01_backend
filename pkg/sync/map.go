b23957b493cafd3abef7560b3c3b077db9978eb9
ras0q <asymptote.k.k@gmail.com>
2022-12-15 16:57:05 +0900

    :recycle: make child field private

--- pkg/sync/map.go
+++ pkg/sync/map.go
@@ -4,15 +4,15 @@ import "sync"
 
 // Map sync.Mapの型付きラッパー
 type Map[K, V any] struct {
-	sync.Map
+	sm sync.Map
 }
 
 func (m *Map[K, V]) Delete(key K) {
-	m.Map.Delete(key)
+	m.sm.Delete(key)
 }
 
 func (m *Map[K, V]) Load(key K) (value V, ok bool) {
-	v, ok := m.Map.Load(key)
+	v, ok := m.sm.Load(key)
 	if !ok {
 		return
 	}
@@ -21,14 +21,14 @@ func (m *Map[K, V]) Load(key K) (value V, ok bool) {
 }
 
 func (m *Map[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
-	v, loaded := m.Map.LoadOrStore(key, value)
+	v, loaded := m.sm.LoadOrStore(key, value)
 	actual = v.(V)
 
 	return
 }
 
 func (m *Map[K, V]) Range(f func(key K, value V) bool) {
-	m.Map.Range(func(k, v any) bool {
+	m.sm.Range(func(k, v any) bool {
 		return f(k.(K), v.(V))
 	})
 }
