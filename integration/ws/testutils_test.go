73fc040d5c8f7b2ccc2f74b1f63ad95f5825f35f
ras0q <asymptote.k.k@gmail.com>
2022-12-15 13:13:29 +0900

    :recycle: httptest.NewServerを使ってwebsocketを接続する

--- integration/ws/testutils_test.go
+++ integration/ws/testutils_test.go
@@ -1,10 +1,13 @@
 package ws_test
 
 import (
+	"net/http"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 	"github.com/hackathon-22-winter-01/hackathon-22-winter-01_backend/internal/oapi"
+	"github.com/hackathon-22-winter-01/hackathon-22-winter-01_backend/internal/usecases/services/ws"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,4 +18,16 @@ func readWsResponse(t *testing.T, c *websocket.Conn) *oapi.WsResponse {
 	require.NoError(t, c.ReadJSON(res))
 
 	return res
+}
+
+type httpHandler struct {
+	t   *testing.T
+	s   ws.Streamer
+	pid uuid.UUID
+}
+
+func (h *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := h.s.ServeWS(w, r, h.pid); err != nil {
+		h.t.Error(err)
+	}
 }
--- integration/ws/ws_test.go
+++ integration/ws/ws_test.go
@@ -1,12 +1,12 @@
 package ws_test
 
 import (
+	"net/http/httptest"
+	"strings"
 	"testing"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
-	"github.com/hackathon-22-winter-01/hackathon-22-winter-01_backend/internal/handler"
 	"github.com/hackathon-22-winter-01/hackathon-22-winter-01_backend/internal/oapi"
 	"github.com/hackathon-22-winter-01/hackathon-22-winter-01_backend/internal/usecases/services/ws"
 	"github.com/labstack/echo/v4"
@@ -19,39 +19,27 @@ func TestWs(t *testing.T) {
 		pids  = make([]uuid.UUID, 4)
 	)
 
-	// 環境変数の設定
-	t.Setenv("APP_PORT", ":8081")
-
-	// APIサーバーを起動
-	e := echo.New()
-	hub := ws.NewHub()
-	st := ws.NewStreamer(hub, e.Logger)
-	h := handler.New(st)
-	oapi.RegisterHandlersWithBaseURL(e, h, "/api/v1")
-
-	go e.Logger.Fatal(e.Start(":8081"))
-
-	time.Sleep(3 * time.Second)
+	// Streamerを起動
+	h := ws.NewHub()
+	s := ws.NewStreamer(h, echo.New().Logger) // TODO: loggerのためにechoを使っているのを直す
 
 	for i := 0; i < 4; i++ {
-		// Websocketクライアントを接続
-		c, _, err := websocket.DefaultDialer.Dial("ws://localhost:8081/api/v1/ws", nil)
-		require.NoError(t, err)
+		pids[i] = uuid.New()
 
-		defer c.Close()
+		// Websocketクライアントを接続
+		server := httptest.NewServer(&httpHandler{t, s, pids[i]})
+		server.URL = "ws" + strings.TrimPrefix(server.URL, "http")
+		c, _, err := websocket.DefaultDialer.Dial(server.URL, nil)
+		require.NoError(t, err)
 
 		conns[i] = c
+		defer c.Close()
 
-		// 接続を確認 & プレイヤーIDを取得
+		// 接続を確認
 		res := readWsResponse(t, c)
-		b, err := new(oapi.WsResponse_Body).AsWsResponseBodyConnected()
+		resbody, err := res.Body.AsWsResponseBodyConnected()
 		require.NoError(t, err)
-
-		pids[i] = b.PlayerId
-
-		expectedResBody := new(oapi.WsResponse_Body)
-		require.NoError(t, expectedResBody.FromWsResponseBodyConnected(oapi.WsResponseBodyConnected{PlayerId: pids[i]}))
 		require.Equal(t, oapi.WsResponseTypeConnected, res.Type)
-		require.Equal(t, expectedResBody, &res.Body)
+		require.Equal(t, oapi.WsResponseBodyConnected{PlayerId: pids[i]}, resbody)
 	}
 }
