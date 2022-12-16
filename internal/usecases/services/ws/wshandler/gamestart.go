450897e0361054ce3097d881999c9faa9aa92999
ras0q <asymptote.k.k@gmail.com>
2022-12-16 15:10:37 +0900

    :sparkles: impl drawcard for test

--- internal/usecases/services/ws/wshandler/gamestart.go
+++ internal/usecases/services/ws/wshandler/gamestart.go
@@ -1,7 +1,6 @@
 package wshandler
 
 import (
-	"github.com/google/uuid"
 	"github.com/hackathon-22-winter-01/hackathon-22-winter-01_backend/internal/oapi"
 	"github.com/hackathon-22-winter-01/hackathon-22-winter-01_backend/pkg/jst"
 )
@@ -12,14 +11,14 @@ func (h *wsHandler) handleGameStartEvent(body oapi.WsRequest_Body) error {
 		return err
 	}
 
-	// TODO: 初期カードを決めるロジックを書く
-	// テスト時は固定する
-	cards := []oapi.Card{
-		{Id: uuid.New(), Type: oapi.CardTypeCreateRail},
-		{Id: uuid.New(), Type: oapi.CardTypeCreateBlock},
-		{Id: uuid.New(), Type: oapi.CardTypeCreateRail},
-		{Id: uuid.New(), Type: oapi.CardTypeCreateBlock},
-		{Id: uuid.New(), Type: oapi.CardTypeCreateRail},
+	domainCards, err := h.cardRepo.DrawCards(h.room.ID, h.playerID, 5)
+	if err != nil {
+		return err
+	}
+
+	cards := make([]oapi.Card, len(domainCards))
+	for i, c := range domainCards {
+		cards[i] = oapi.CardFromDomain(c)
 	}
 
 	players := make([]oapi.Player, len(h.room.Players))
@@ -32,6 +31,7 @@ func (h *wsHandler) handleGameStartEvent(body oapi.WsRequest_Body) error {
 		return err
 	}
 
+	// TODO: 全員に同じカードが渡ってしまう
 	if err := h.sender.Broadcast(h.room.ID, res); err != nil {
 		return err
 	}
